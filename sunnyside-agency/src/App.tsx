4f1cc8d6652a777824ae4dac638072b74e73876c
Amanuel Haile <amanuelhaile369@gmail.com>
2026-03-13 11:14:49 +0300

    add Testimonials commponent to the App.tsx

--- sunnyside-agency/src/App.tsx
+++ sunnyside-agency/src/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from "./componentes/Header";
+import { Services } from "./componentes/Services";
+import { Testimonials } from "./componentes/Testimonials";
 
 function App() {
   return (
     <>
-      <div className="w-full h-svh">
+      <div className="w-full h-svh font-barlow">
         <Header />
+        <main>
+          <Services />
+          <Testimonials />
+        </main>
       </div>
     </>
   );
