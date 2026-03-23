9a489309f779a1024356982e321896a3a0c4cad1
Amanuel Haile <amanuelhaile369@gmail.com>
2026-03-23 17:47:50 +0300

    initial commit for manage

---
+++ manage/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <section id="center" className="h-svh flex justify-center items-center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
