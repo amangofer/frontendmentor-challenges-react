7682ec0aca46e6f9fe6763121c0f0cd9ec4b17c4
Amanuel Haile <amanuelhaile369@gmail.com>
2026-04-13 22:04:26 +0300

    digital-bank-landing-page starter

---
+++ digital-bank-landing-page/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-svh flex justify-center items-center">
+      <h1 className="text-4xl">Digital Bank</h1>
+    </div>
+  );
+}
+
+export default App;
